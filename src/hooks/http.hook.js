675dfa27b3358f2f43add873cef8fa5bf6db8920
Bohdan Solonukha <bodyasolo@gmail.com>
2022-08-12 10:53:58 +0300

    using the custom useMarvelService hook in the component RandomChar

--- src/hooks/http.hook.js
+++ src/hooks/http.hook.js
@@ -9,17 +9,18 @@ export const useHttp = () => {
     try {
       const response = await fetch(url, { method, body, headers });
       if (!response.ok) {
-        throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+        throw new Error(`Could not fetch ${url}, status: ${response.status}`);
       }
-      const data = await response.json;
+      const data = await response.json();
       setLoading(false);
+      return data;
     } catch (e) {
       setLoading(false);
       setError(e.message);
       throw e;
     }
     
-  }, [])
+  }, []);
 
   const clearError = useCallback(() => setError(null), []);
 
