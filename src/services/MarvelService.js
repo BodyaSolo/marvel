9024d41b9787cc1bfa6617163a55820cb99025e4
Bohdan Solonukha <bodyasolo@gmail.com>
2022-08-18 13:18:40 +0300

    error correction

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -13,7 +13,7 @@ const useMarvelService = () => {
 
   const getCharacterByName = async (name) => {
     const res = await request(`${_apiBase}characters?name=${name}&${_apiKey}`);
-    return _transformCharacter(res.data.results[0]);
+    return res.data.results.map(_transformCharacter);
   }
 
   const getCharacter = async (id) => {
