55049eea9aafdcd0969e3b7bd2c220180f954384
Bohdan Solonukha <bodyasolo@gmail.com>
2022-08-18 13:18:18 +0300

    using Formil and Yup for Search form

--- src/components/charSearchForm/CharSearchForm.js
+++ src/components/charSearchForm/CharSearchForm.js
@@ -27,28 +27,44 @@ const CharSearchForm = () => {
   const results = !char ? null : char.length > 0 ? 
                 <div>
                   <div className="char__search-success">There is! Visit Thor page?</div>
-                  <button className="button button__secondary">
+                  <Link to={`/characters/${char[0].id}`} className="button button__secondary">
                     <div className="inner">To page</div>
-                  </button>
+                  </Link>
                 </div> :
                 <div className="char__search-error">The character was not found. Check the name and try again</div>
 
 
   return (
     <div className="char__search-form">
-      <form>
-        <label className="char__search-label" htmlFor="charName">Or find a character by name:</label>
-        <div className="char__search-wrapper">
-          <input
-          id="charName"
-          name="charName"
-          type="text"
-          placeholder="Enter name"/>
-          <button className="button button_main" type="submit">
-            <div className="inner">find</div>
-          </button>
-        </div>
-      </form>
+      <Formik
+        initialValues = {{
+          charName: ''
+        }}
+        validationSchema = {Yup.object({
+          charName: Yup.string().required('This field is required')
+        })}
+        onSubmit={({ charName }) => {
+          updateChar(charName);
+        }}
+      >
+        <Form>
+          <label className="char__search-label" htmlFor="charName">Or find a character by name:</label>
+          <div className="char__search-wrapper">
+            <Field
+              id="charName"
+              name="charName"
+              type="text"
+              placeholder="Enter name"/>
+              <button
+                className="button button_main"
+                type="submit"
+                disabled={loading}>
+              <div className="inner">find</div>
+            </button>
+          </div>
+          <FormikErrorMessage component="div" className="char__search-error" name="charName"/>
+        </Form>
+      </Formik>
       {results}
       {errorMessage}
     </div>
